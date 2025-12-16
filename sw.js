7482c4cb3cf0d1c854ae5a86eb8e563844377d09
gazmannn0890-cyber <gazmannn0890@gmail.com>
2025-12-16 11:12:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,58 @@
+const CACHE_NAME = 'telegram-calls-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://i.pravatar.cc/150?img=1',
+  'https://i.pravatar.cc/150?img=5',
+  'https://i.pravatar.cc/150?img=8'
+];
 
+// Установка Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация и очистка старых кэшей
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Стратегия кэширования: Network First, затем Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Клонируем ответ для кэширования
+        const responseToCache = response.clone();
+        
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+        
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
